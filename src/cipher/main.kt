0d8b4aa75ca8112c3b950a90139821067531827a
gferra02 <gferra02@mail.bbk.ac.uk>
2018-03-07 17:55:52 +0000

    Setting up for IntelliJ

---
+++ src/cipher/main.kt
@@ -0,0 +1,5 @@
+package cipher
+
+fun main(args: Array<String>) {
+    println("just testing Intellij setup")
+}
---
+++ src/test/simpleTest.kt
@@ -0,0 +1,14 @@
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Ignore
+
+class simpleTest {
+
+//    @Before
+//
+//    @Ignore
+//    @Test
+//
+//    @After
+}
