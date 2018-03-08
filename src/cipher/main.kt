c67b17393bc9830ddae2eb6c9a98dfb5216dbeb7
gferra02 <gferra02@mail.bbk.ac.uk>
2018-03-08 21:44:25 +0000

    Latest version of micro tests

--- src/cipher/main.kt
+++ src/cipher/main.kt
@@ -1,8 +1,8 @@
 package cipher
 
 fun main(args: Array<String>) {
-    println(encipher("Hello World", 1));
-    println(encipher("Zebra", 14));
-    println(decipher(encipher("The quick brown fox jumps over the lazy dog", 9)));
+    println(encipher("Hello World", 1))
+    println(encipher("Zebra", 14))
+    println(decipher(encipher("The quick brown fox jumps over the lazy dog", 9)))
 }
 
