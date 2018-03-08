b389742ac7b6f2684de700f25300ec660565997b
gferra02 <gferra02@mail.bbk.ac.uk>
2018-03-08 19:54:08 +0000

    Update decipher to calculate a score by looking at character frequency

--- src/cipher/decipher.kt
+++ src/cipher/decipher.kt
@@ -1,5 +1,34 @@
 package cipher
 
 fun decipher(str: String): String {
- return encipher(str, (Math.random() * 25).toInt());
+    // Generate every variation of the string
+    val variations = Array(26,  { i -> encipher(str, i)})
+
+    // Calculate a score for each possible variation
+    val scores = Array(26, { i -> calculateFrequencyScore(variations.get(i)) })
+
+    var maxIndex = 0;
+    variations.forEachIndexed { index, str ->
+        if (scores[index] > scores[maxIndex]) {
+            maxIndex = index;
+        }
+    }
+
+    // Return the string with the highest score
+    return variations[maxIndex];
+}
+
+// Modern English Letter Frequencies
+// Source: http://www.math.ucsd.edu/~crypto/Projects/MarshaMoreno/TimeComparisonFrequency.pdf
+val letters = "eothasinrdluymwfgcbpkvjqxz".reversed()
+
+// Calculate a score for a string based on the frequency of particular letters
+fun calculateFrequencyScore(str: String): Int {
+    var score: Int = 0;
+
+    for (i in 0..(str.length - 1)) score += letters.indexOf(
+        str[i].toLowerCase()
+    )
+
+    return score;
 }
--- src/cipher/main.kt
+++ src/cipher/main.kt
@@ -3,6 +3,6 @@ package cipher
 fun main(args: Array<String>) {
     println(encipher("Hello World", 1));
     println(encipher("Zebra", 14));
-    println(decipher(encipher("Hello World", 1)));
+    println(decipher(encipher("The quick brown fox jumps over the lazy dog", 9)));
 }
 
