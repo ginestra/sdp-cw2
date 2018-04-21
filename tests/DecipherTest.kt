1a172d88eef2acd8e290443f48c4988d64dceeec
gferra02 <gferra02@mail.bbk.ac.uk>
2018-04-21 17:28:30 +0100

    Added test for longer sentences in cipher and for offset value

--- tests/DecipherTest.kt
+++ tests/DecipherTest.kt
@@ -11,5 +11,9 @@ class DecipherTest {
             decipher(encipher("The quick brown fox jumps over the lazy dog", 9)),
             "The quick brown fox jumps over the lazy dog"
         )
+        assertEquals(
+                decipher(encipher("The pen is on the table, the cat is under the chair", 9)),
+                "The pen is on the table, the cat is under the chair"
+        )
     }
 }
--- tests/EncipherTest.kt
+++ tests/EncipherTest.kt
@@ -6,7 +6,20 @@ import cipher.encipher
 class EncipherTest {
     @org.junit.Test
     fun execute() {
-        assertEquals(encipher("Hello World", 1), "Ifmmp Xpsme");
+        assertEquals(encipher("Hello World", 1), "Ifmmp Xpsme")
         assertEquals(encipher("Zebra", 14), "Nspfo")
+        assertEquals(encipher("The pen is on the table", 3), "Wkh shq lv rq wkh wdeoh")
+        assertEquals(encipher("The quick fox jumps over the lazy dog", 7),
+                "Aol xbpjr mve qbtwz vcly aol shgf kvn")
+    }
+
+    @org.junit.Test
+    fun testOffsetException() {
+        try {
+            encipher("Hello World", 26)
+            fail("Expected an IllegalArgumentException to be thrown")
+        } catch (exception: IllegalArgumentException) {
+            assertTrue(exception.message.equals("offset must be between 0 and 25"))
+        }
     }
 }
